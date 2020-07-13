759766416dfcec0f0ae0b1378ccc187bc95b4aaa
Elias <eliasgarciadev@gmail.com>
2020-07-13 11:03:13 +0200

    Refactor app component

--- src/views/app.jsx
+++ src/views/app.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import './app.css'
 import InitView from './auth/init-view'
 import ActionView from './transactions/action-view'
 
-class App extends Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <Route
-          exact
-          path='/'
-          component={InitView}
-        />
-        <Route
-          exact
-          path='/actions'
-          component={ActionView}
-        />
-      </BrowserRouter>
-    )
-  }
+function App () {
+  return (
+    <BrowserRouter>
+      <Route
+        exact
+        path='/'
+        component={InitView}
+      />
+      <Route
+        exact
+        path='/actions'
+        component={ActionView}
+      />
+    </BrowserRouter>
+  )
 }
 
 export default (App)
