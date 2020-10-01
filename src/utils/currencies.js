c9c5b20fd0499635565d3d4ea5ab02aab815dc29
Elias <eliasgarciadev@gmail.com>
2020-10-01 17:53:18 +0200

    Add comments and fix PR comments

--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -1,3 +1,5 @@
+import { MAX_DECIMALS_UNTIL_ZERO_AMOUNT } from '../constants'
+
 const CurrencySymbol = {
   USD: {
     symbol: '$',
@@ -9,25 +11,39 @@ const CurrencySymbol = {
   }
 }
 
-function getFixedTokenAmount (balance, decimals) {
-  const balanceWithDecimals = Number(balance) / Math.pow(10, decimals)
+/**
+ * Gets the string representation of a token amount with fixed decimals
+ * @param {string} amount - Amount to be be converted in a bigint format
+ * @param {number} decimals - Decimals that the amount should have in its string representation
+ * @returns {string}
+ */
+function getFixedTokenAmount (amount, decimals) {
+  const balanceWithDecimals = Number(amount) / Math.pow(10, decimals)
 
-  if (balanceWithDecimals < 10 / (Math.pow(10, 7))) {
+  if (balanceWithDecimals < 1 / (Math.pow(10, MAX_DECIMALS_UNTIL_ZERO_AMOUNT))) {
     return '0'
   }
 
   return balanceWithDecimals.toFixed(decimals)
 }
 
+/**
+ * Converts a token amount to a new amount but in the user preferred currency
+ * @param {string} amount - The amount to be be converted
+ * @param {string} usdTokenExchangeRate - Current USD exchange rate for the token
+ * @param {string} preferredCurrency - User preferred currency
+ * @param {string} fiatExchangeRates - Exchange rates for all the supported currencies in the app
+ * @returns {string}
+ */
 function getTokenAmountInPreferredCurrency (
   amount,
   usdTokenExchangeRate,
   preferredCurrency,
-  fiatExchangeRatesTask
+  fiatExchangeRates
 ) {
   const usdAmount = Number(amount) * usdTokenExchangeRate
 
-  if (!fiatExchangeRatesTask) {
+  if (!fiatExchangeRates) {
     return undefined
   }
 
@@ -35,7 +51,7 @@ function getTokenAmountInPreferredCurrency (
     return usdAmount
   }
 
-  return usdAmount * fiatExchangeRatesTask[preferredCurrency]
+  return usdAmount * fiatExchangeRates[preferredCurrency]
 }
 
 export {
