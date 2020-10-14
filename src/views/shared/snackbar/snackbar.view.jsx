70b0e539ebf89f24c7d64c14113336eeb747bebe
Elias <eliasgarciadev@gmail.com>
2020-10-14 11:32:30 +0200

    Fix snackbar memory leak

--- src/views/shared/snackbar/snackbar.view.jsx
+++ src/views/shared/snackbar/snackbar.view.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import Container from '../container/container.view'
 import useSnackbarStyles from './snackbar.styles'
 import { SNACKBAR_AUTO_HIDE_DURATION } from '../../../constants'
 
-function Snackbar ({ show, message, onClose }) {
+function Snackbar ({ message, onClose }) {
   const classes = useSnackbarStyles()
+  const [closingTimeoutId, setClosingTimeoutId] = useState()
 
   React.useEffect(() => {
-    if (show) {
-      setTimeout(() => {
-        onClose()
-      }, SNACKBAR_AUTO_HIDE_DURATION)
-    }
-  }, [show, onClose])
-
-  return show
-    ? (
-      <div className={classes.root}>
-        <Container disableVerticalGutters>
-          <div className={classes.wrapper}>
-            <p className={classes.message}>{message}</p>
-          </div>
-        </Container>
-      </div>
+    setClosingTimeoutId(
+      setTimeout(onClose, SNACKBAR_AUTO_HIDE_DURATION)
     )
-    : <></>
+
+    return () => {
+      clearTimeout(closingTimeoutId)
+    }
+  }, [onClose])
+
+  return (
+    <div className={classes.root}>
+      <Container disableVerticalGutters>
+        <div className={classes.wrapper}>
+          <p className={classes.message}>{message}</p>
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 Snackbar.propTypes = {
-  show: PropTypes.bool.isRequired,
   message: PropTypes.string.isRequired,
   onClose: PropTypes.func
 }
