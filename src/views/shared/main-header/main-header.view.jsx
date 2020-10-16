8b80e4e3c5657a0180491b34e79a8e854b4d5b4c
Elias <eliasgarciadev@gmail.com>
2020-10-16 11:20:28 +0200

    Hide the QR feature from the header

--- src/views/shared/main-header/main-header.view.jsx
+++ src/views/shared/main-header/main-header.view.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import useMainHeaderStyles from './main-header.styles'
 import hermezLogo from '../../../images/hermez-logo.svg'
 import userAccountIcon from '../../../images/icons/user-account.svg'
-import qrCodeIcon from '../../../images/icons/qr-code.svg'
+// import qrCodeIcon from '../../../images/icons/qr-code.svg'
 import Container from '../container/container.view'
 
 function MainHeader () {
@@ -27,10 +27,10 @@ function MainHeader () {
             </Link>
           </h1>
         </div>
-        <Link to='/my-address' className={`${classes.link} ${classes.addressLink}`}>
+        {/* <Link to='/my-address' className={`${classes.link} ${classes.addressLink}`}>
           <p className={classes.linkText}>My address</p>
           <img src={qrCodeIcon} alt='Settings' className={classes.addressIcon} />
-        </Link>
+        </Link> */}
       </Container>
     </header>
   )
