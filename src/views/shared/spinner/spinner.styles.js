9fdf455187c7708f423e89658fb40c2e8c66dff0
Elias <eliasgarciadev@gmail.com>
2020-10-20 16:18:05 +0200

    Add a CSS only Spinner

---
+++ src/views/shared/spinner/spinner.styles.js
@@ -0,0 +1,25 @@
+import { createUseStyles } from 'react-jss'
+
+const useSpinnerStyles = createUseStyles(theme => ({
+  '@keyframes spin': {
+    from: { transform: 'rotate(0deg)' },
+    to: { transform: 'rotate(360deg)' }
+  },
+  root: ({ size }) => ({
+    width: size,
+    height: size,
+    animation: '$spin 0.8s linear infinite'
+  }),
+  topCircle: {
+    stroke: theme.palette.orange.main,
+    strokeLinecap: 'round',
+    strokeDasharray: '30px 200px',
+    strokeDashoffset: '0px'
+  },
+  bottomCircle: {
+    stroke: theme.palette.orange.main,
+    strokeOpacity: 0.2
+  }
+}))
+
+export default useSpinnerStyles
--- src/views/shared/spinner/spinner.view.jsx
+++ src/views/shared/spinner/spinner.view.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Spinner () {
-  return <p>Loading...</p>
+import useSpinnerStyles from './spinner.styles'
+
+const SIZE = 44
+const THICKNESS = 6
+
+function Spinner ({ size }) {
+  const classes = useSpinnerStyles({ size })
+
+  return (
+    <svg
+      className={classes.root}
+      viewBox={`${SIZE / 2} ${SIZE / 2} ${SIZE} ${SIZE}`}
+    >
+      <circle
+        className={classes.bottomCircle}
+        cx={SIZE}
+        cy={SIZE}
+        r={(SIZE - THICKNESS) / 2}
+        fill='none'
+        strokeWidth={THICKNESS}
+      />
+      <circle
+        className={classes.topCircle}
+        cx={SIZE}
+        cy={SIZE}
+        r={(SIZE - THICKNESS) / 2}
+        fill='none'
+        strokeWidth={THICKNESS}
+      />
+    </svg>
+  )
+}
+
+Spinner.propTypes = {
+  size: PropTypes.number.isRequired
 }
 
 export default Spinner
