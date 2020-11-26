74e61485752d39365af5b80d7d31ec9be8591e10
Elias <eliasgarciadev@gmail.com>
2020-11-26 14:26:35 +0100

    Add comment on container styles

--- src/views/shared/container/container.styles.js
+++ src/views/shared/container/container.styles.js
@@ -12,6 +12,7 @@ const useContainerStyles = createUseStyles(theme => ({
     width: '100%',
     height: '100%',
     display: 'flex',
+    // 2 * theme.spacing(3.5) is the sides padding, so the real width is 700px
     maxWidth: 700 + 2 * theme.spacing(3.5),
     margin: 'auto',
     padding: ({ disableGutters, disableVerticalGutters, disableTopGutter }) =>
