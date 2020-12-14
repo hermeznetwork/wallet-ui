440e35a90a103f3e333353ba454e5a32f726f767
Elias <eliasgarciadev@gmail.com>
2020-12-14 20:07:00 +0100

    Update code comments

--- src/utils/hw-wallets.js
+++ src/utils/hw-wallets.js
@@ -5,9 +5,10 @@ import TrezorConnect from 'trezor-connect'
 import { strToHex } from './strings'
 
 /**
- *
+ * Builds an Ethereum BIP-44 path from an account type and index
  * @param {number} accountType - Account type
  * @param {number} accountIndex - Account index
+ * @returns {string} - BIP-44 path of the account
  */
 function buildEthereumBIP44Path (accountType, accountIndex) {
   return `m/44'/60'/${accountType}'/0/${accountIndex}`
