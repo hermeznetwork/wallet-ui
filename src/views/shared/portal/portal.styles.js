d99db70fcea1d7cdb7e212933607ff9642eb0665
Elias <eliasgarciadev@gmail.com>
2021-01-03 03:35:42 +0100

    Fix Portal warning being displayed to the console

--- src/views/shared/portal/portal.styles.js
+++ src/views/shared/portal/portal.styles.js
@@ -1,6 +1,6 @@
 import { createUseStyles } from 'react-jss'
 
-const usePortalStyles = createUseStyles(() => ({
+const usePortalStyles = createUseStyles({
   root: {
     position: 'absolute',
     top: 0,
@@ -8,6 +8,6 @@ const usePortalStyles = createUseStyles(() => ({
     right: 0,
     bottom: 0
   }
-}))
+})
 
 export default usePortalStyles
