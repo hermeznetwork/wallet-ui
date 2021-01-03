02fd2f5c4a140af703f1a0795edf6d21d15d4de4
Elias <eliasgarciadev@gmail.com>
2021-01-03 03:48:29 +0100

    Fix Portal bug not being displayed properly when reducing the screen to the half of the height

--- src/views/shared/portal/portal.styles.js
+++ src/views/shared/portal/portal.styles.js
@@ -2,7 +2,7 @@ import { createUseStyles } from 'react-jss'
 
 const usePortalStyles = createUseStyles({
   root: {
-    position: 'absolute',
+    position: 'fixed',
     top: 0,
     left: 0,
     right: 0,
