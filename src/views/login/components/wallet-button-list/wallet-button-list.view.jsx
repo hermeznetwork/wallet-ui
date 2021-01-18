daa9442dc5bcf81075e8bdfaad46f85c1c48d2f7
Elias <eliasgarciadev@gmail.com>
2021-01-18 11:25:28 +0100

    Add hw wallets support again

--- src/views/login/components/wallet-button-list/wallet-button-list.view.jsx
+++ src/views/login/components/wallet-button-list/wallet-button-list.view.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import useWalletButtonListStyles from './wallet-button-list.styles'
 import WalletButton from '../wallet-button/wallet-button.view'
+import { WalletName } from '../../login.view'
 
 function WalletButtonList ({ onClick }) {
   const classes = useWalletButtonListStyles()
@@ -11,8 +12,18 @@ function WalletButtonList ({ onClick }) {
       <div className={classes.walletButtonContainer}>
         <WalletButton
           isClickable
-          walletName='metaMask'
-          onClick={() => onClick('metaMask')}
+          walletName={WalletName.METAMASK}
+          onClick={() => onClick(WalletName.METAMASK)}
+        />
+        <WalletButton
+          isClickable
+          walletName={WalletName.LEDGER}
+          onClick={() => onClick(WalletName.LEDGER)}
+        />
+        <WalletButton
+          isClickable
+          walletName={WalletName.TREZOR}
+          onClick={() => onClick(WalletName.TREZOR)}
         />
       </div>
     </div>
