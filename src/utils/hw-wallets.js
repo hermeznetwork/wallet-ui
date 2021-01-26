d8fc29583845fce049645e18ff73763349b604cb
Elias <eliasgarciadev@gmail.com>
2021-01-26 10:55:37 +0100

    Update buildEthereumBIP44Path comments

--- src/utils/hw-wallets.js
+++ src/utils/hw-wallets.js
@@ -1,5 +1,7 @@
 /**
- * Builds an Ethereum BIP-44 path from an account type and index
+ * Builds an Ethereum BIP-44 path from a chain id, account type and index. The chain id
+ * is used to determine the coin type of the BIP-44 path. 60 is used for Mainnet and 1
+ * is used for Testnets.
  * @param {number} accountType - Account type
  * @param {number} accountIndex - Account index
  * @returns {string} - BIP-44 path of the account
