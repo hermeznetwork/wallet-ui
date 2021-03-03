5af79b2b25113637c8d7d2f607c687c411f4d0de
Elias <eliasgarciadev@gmail.com>
2021-03-03 17:47:31 +0100

    Improve the onboarding when accessing from a mobile

--- src/views/login/components/metamask-error/metamask-error.view.jsx
+++ src/views/login/components/metamask-error/metamask-error.view.jsx
@@ -3,16 +3,34 @@ import React from 'react'
 import useMetaMaskErrorStyles from './metamask-error.styles'
 import { WalletName } from '../../login.view'
 import WalletButton from '../wallet-button/wallet-button.view'
+import { isAndroidDevice, isiOsDevice, isMobileDevice } from '../../../../utils/browser'
 
 function MetaMaskError () {
   const classes = useMetaMaskErrorStyles()
 
+  function getMetaMaskDownloadLink () {
+    if (isAndroidDevice()) {
+      return 'https://play.google.com/store/apps/details?id=io.metamask'
+    }
+    if (isiOsDevice()) {
+      return 'itms-apps://itunes.apple.com/app/id1438144202'
+    }
+
+    return 'https://metamask.io/download.html'
+  }
+
   return (
     <div className={classes.root}>
-      <h1 className={classes.errorTitle}>Install MetaMask to continue</h1>
+      <h1 className={classes.errorTitle}>
+        {
+          isMobileDevice()
+            ? 'Install MetaMask app and access through the MetaMask browser'
+            : 'Install MetaMask to continue'
+        }
+      </h1>
       <WalletButton walletName={WalletName.METAMASK} hideName />
       <a
-        href='https://metamask.io/download.html'
+        href={getMetaMaskDownloadLink()}
         target='_blank'
         rel='noopener noreferrer'
         className={classes.href}
