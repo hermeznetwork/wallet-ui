2e8109ced3a7949855347eef5d88593f265cca13
Elias <eliasgarciadev@gmail.com>
2021-03-06 22:47:25 +0100

    Add a copy button to the hez eth address in TransactionInfo

--- src/views/shared/transaction-info-table-row/transaction-info-row.view.jsx
+++ src/views/shared/transaction-info-table-row/transaction-info-row.view.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import useTransactionInfoRowStyles from './transaction-info-row.styles'
+import { ReactComponent as CopyIcon } from '../../../images/icons/copy.svg'
 
-function TransactionInfoRow ({ title, subtitle, value }) {
+function TransactionInfoRow ({ title, subtitle, value, showCopyButton, onCopySubtitle }) {
   const classes = useTransactionInfoRowStyles()
 
   return (
     <div className={classes.root}>
       <p className={classes.title}>{title}</p>
       <div className={classes.content}>
-        <p className={classes.subtitle}>{subtitle}</p>
+        <div className={classes.subtitle}>
+          {
+            showCopyButton && (
+              <button className={classes.copyButton} onClick={onCopySubtitle}>
+                <CopyIcon className={classes.copyIcon} />
+              </button>
+            )
+          }
+          <p>{subtitle}</p>
+        </div>
         {value ? <p className={classes.value}>{value}</p> : <p className={classes.value}>&nbsp;</p>}
       </div>
     </div>
