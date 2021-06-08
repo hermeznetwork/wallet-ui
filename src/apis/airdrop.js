59c145d268de2fa1bfd31074b754bcbe93b32002
Miros <miros@iden3.com>
2021-06-08 17:02:22 +0200

    minor refactor

--- src/apis/airdrop.js
+++ src/apis/airdrop.js
@@ -2,15 +2,12 @@ import axios from 'axios'
 
 import { HERMEZ_AIRDROP_WEB_URL, HERMEZ_AIRDROP_ID } from '../constants'
 
-const baseApiUrl = HERMEZ_AIRDROP_WEB_URL
-const airdropID = HERMEZ_AIRDROP_ID
-
 /**
  * Fetches Airdrop estimated reward
  * @returns {Number} - estimatedReward
  */
 function getEstimatedReward (ethAddr) {
-  return axios.get(`${baseApiUrl}/estimated-reward?ethAddr=${ethAddr}&airdropID=${airdropID}`)
+  return axios.get(`${HERMEZ_AIRDROP_WEB_URL}/estimated-reward?ethAddr=${ethAddr}&airdropID=${HERMEZ_AIRDROP_ID}`)
     .then(res => res.data.estimatedReward)
 }
 
@@ -19,7 +16,7 @@ function getEstimatedReward (ethAddr) {
  * @returns {Number} - earnedReward
  */
 function getEarnedReward (ethAddr) {
-  return axios.get(`${baseApiUrl}/earned-reward?ethAddr=${ethAddr}&airdropID=${airdropID}`)
+  return axios.get(`${HERMEZ_AIRDROP_WEB_URL}/earned-reward?ethAddr=${ethAddr}&airdropID=${HERMEZ_AIRDROP_ID}`)
     .then(res => res.data.earnedReward)
 }
 
