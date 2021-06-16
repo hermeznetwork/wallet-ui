5d9d671c3460b1aece3313725f42fb9d8375eed7
Elias <eliasgarciadev@gmail.com>
2021-06-16 13:27:46 +0200

    Remove duplicated text

--- src/views/shared/rewards-sidenav/rewards-sidenav.view.jsx
+++ src/views/shared/rewards-sidenav/rewards-sidenav.view.jsx
@@ -177,14 +177,6 @@ function RewardsSidenav ({
                     }
                   </div>
                 </div>
-
-                <div className={classes.infoTextWrapper}>
-                  <InfoGreyIcon className={classes.infoIcon} />
-                  <p className={classes.infoText}>
-                    You will receive your reward at the end of the program.
-                  </p>
-                </div>
-
                 {
                   accountEligibilityTask.data && (
                     <div className={classes.infoTextWrapper}>
