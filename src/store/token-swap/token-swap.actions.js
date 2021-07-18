422e08205d470b2e82587201ecc66d25695d02a9
Elias <eliasgarciadev@gmail.com>
2021-07-19 00:51:07 +0200

    Add token swap stepper

--- src/store/token-swap/token-swap.actions.js
+++ src/store/token-swap/token-swap.actions.js
@@ -1,5 +1,19 @@
 export const tokenSwapActionTypes = {
+  GO_TO_SWAP: '[TOKEN SWAP] GO TO SWAP',
+  GO_TO_QUOTES: '[TOKEN SWAP] GO TO QUOTES',
   RESET_STATE: '[TOKEN SWAP] RESET STATE'
+}
+
+function goToSwap () {
+  return {
+    type: tokenSwapActionTypes.GO_TO_SWAP
+  }
+}
+
+function goToQuotes () {
+  return {
+    type: tokenSwapActionTypes.GO_TO_QUOTES
+  }
 }
 
 function resetState () {
@@ -9,5 +23,7 @@ function resetState () {
 }
 
 export {
+  goToSwap,
+  goToQuotes,
   resetState
 }
