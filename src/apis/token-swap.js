f13e6058ae9d0e64bafd3e84fec063c77f0de98b
Adri <19190248+adrifdez@users.noreply.github.com>
2021-08-02 11:13:54 +0200

    Add api docs

--- src/apis/token-swap.js
+++ src/apis/token-swap.js
@@ -4,7 +4,13 @@ const baseApiUrl = `${process.env.REACT_APP_TOKEN_SWAP_API_URL}`
 
 /**
  * Get quotes from liquidity providers
- * @param {Object} request - There should be either amountToToken or
+ * @typedef {Object} request
+ * @property {string} fromToken - contract address from Token that want to Swap
+ * @property {string} toToken - contract addres from Token that want to Get
+ * @property {string} fromHezAddr - address with tokens to swap
+ * @prop {string} [amountFromToken] - amount that want to swap
+ * @prop {string} [amountToToken] - amount that want to receive
+ * @param {request} request - There should be either amountToToken or
  * amountFromToken field in request set, not both. In first case client
  * declares the amount of token he wants to get. In the last case, client
  *  specifies the amount he has for exchange.
