eed58bdd369ecb2f3c8dff68d7b79ef550b7afce
Alex Monso <amonsosanz@gmail.com>
2021-08-25 09:25:46 +0200

    Migrate fiat-exchange-rates API to typescript

--- src/apis/fiat-exchange-rates.ts
+++ src/apis/fiat-exchange-rates.ts
@@ -3,6 +3,16 @@ import axios from 'axios'
 import { CurrencySymbol } from '../utils/currencies'
 import { FIAT_EXCHANGE_RATES_API_URL } from '../constants'
 
+// domain
+import { FiatExchangeRates } from '../domain'
+
+export interface ApiExchangeRate {
+  currency: string;
+  baseCurrency: string;
+  price: number;
+  lastUpdate: Date;
+}
+
 const baseApiUrl = FIAT_EXCHANGE_RATES_API_URL
 
 const mockedFiatExchangeRates = {
@@ -15,15 +25,16 @@ const mockedFiatExchangeRates = {
 /**
  * Fetches the USD exchange rates for the requested currency symbols
  * @param {string[]} symbols - ISO 4217 currency codes
- * @returns {Object} - USD exchange rates for the requested symbols
+ * @returns {Promise<FiatExchangeRates>} - USD exchange rates for the requested symbols
  */
 // eslint-disable-next-line no-unused-vars
-function getFiatExchangeRates (symbols) {
+function getFiatExchangeRates (symbols: string[]): Promise<FiatExchangeRates> {
   const params = { base: CurrencySymbol.USD.code, symbols: symbols.join('|') }
 
   return axios.get(`${baseApiUrl}/currencies`, { params })
     .then((res) => {
-      return res.data.currencies.reduce((acc, rate) => ({
+      const currencies: ApiExchangeRate[] = res.data.currencies;
+      return currencies.reduce((acc, rate) => ({
         ...acc,
         [rate.currency]: rate.price
       }), {})
