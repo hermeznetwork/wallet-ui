c480880a06a2d60619aa4d1be24d22f11c2afc05
Alex Monso <amonsosanz@gmail.com>
2021-09-10 18:14:51 +0200

    Fix eslint errors in hermez-web.ts

--- src/apis/hermez-web.ts
+++ src/apis/hermez-web.ts
@@ -1,17 +1,36 @@
 import axios from "axios";
+import Ajv, { JTDSchemaType } from "ajv/dist/jtd";
 
 import { HERMEZ_WEB_URL } from "src/constants";
 
 const baseApiUrl = HERMEZ_WEB_URL;
+
+const ajv = new Ajv();
+
+interface WalletUnderMaintenance {
+  isWalletUnderMaintenance: number;
+}
+
+const walletUnderMaintenanceSchema: JTDSchemaType<WalletUnderMaintenance> = {
+  properties: {
+    isWalletUnderMaintenance: { type: "int32" },
+  },
+};
+
+const isWalletUnderMaintenanceValidator = ajv.compile(walletUnderMaintenanceSchema);
 
 /**
  * Fetches the status of the Hermez network
  * @returns {Promise<Number>} - Network status of the Hermez Network
  */
 function getNetworkStatus(): Promise<number> {
-  return axios
-    .get(`${baseApiUrl}/network-status.json`)
-    .then((res) => res.data.isWalletUnderMaintenance);
+  return axios.get(`${baseApiUrl}/network-status.json`).then((res: unknown) => {
+    if (isWalletUnderMaintenanceValidator(res)) {
+      return res.isWalletUnderMaintenance;
+    } else {
+      return Promise.reject(isWalletUnderMaintenanceValidator.errors);
+    }
+  });
 }
 
 export { getNetworkStatus };
