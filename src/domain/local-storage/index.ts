ec58212c3c042a6930c112f1784f17bb7afe3ea8
Alex Monso <amonsosanz@gmail.com>
2021-09-14 18:23:07 +0200

    Improve local storage domain types

--- src/domain/local-storage/index.ts
+++ src/domain/local-storage/index.ts
@@ -1,10 +1,12 @@
 import { Deposit, Withdraw, DelayedWithdraw } from "src/domain/hermez";
 
-type ChainId = string;
-type HermezEthereumAddress = string;
-export type PendingWithdraws = Record<ChainId, Record<HermezEthereumAddress, Withdraw[]>>;
-export type PendingDelayedWithdraws = Record<
-  ChainId,
-  Record<HermezEthereumAddress, DelayedWithdraw[]>
->;
+export type ChainId = string;
+export type HermezEthereumAddress = string;
+
+export type PendingWithdraws = Record<ChainId, ChainPendingWithdraws>;
+export type ChainPendingWithdraws = Record<HermezEthereumAddress, Withdraw[]>;
+
+export type PendingDelayedWithdraws = Record<ChainId, ChainPendingDelayedWithdraws>;
+export type ChainPendingDelayedWithdraws = Record<HermezEthereumAddress, DelayedWithdraw[]>;
+
 export type PendingDeposits = Record<ChainId, Record<HermezEthereumAddress, Deposit[]>>;
