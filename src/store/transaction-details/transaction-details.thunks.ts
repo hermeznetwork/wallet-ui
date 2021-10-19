766bcf523b7cccde5aff17abc82ed8d92787dfe5
Alex Monso <amonsosanz@gmail.com>
2021-10-19 14:43:15 +0200

    Refactor transaction-details thunks to Typescript

---
+++ src/store/transaction-details/transaction-details.thunks.ts
@@ -0,0 +1,77 @@
+import { CoordinatorAPI } from "@hermeznetwork/hermezjs";
+import { HttpStatusCode } from "@hermeznetwork/hermezjs/src/http";
+import { push } from "connected-react-router";
+
+import { AppState, AppDispatch, AppThunk } from "src/store";
+import * as transactionDetailsActionTypes from "./transaction-details.actions";
+import * as storage from "../../utils/storage";
+// domain
+import { PendingDeposit, HistoryTransaction, PooledTransaction } from "src/domain/hermez";
+import { AxiosError } from "axios";
+
+/**
+ * Fetches the details of a transaction
+ * @param {string} transactionIdOrHash - HistoryTransaction id or hash
+ */
+function fetchTransaction(transactionIdOrHash: string): AppThunk {
+  return (dispatch: AppDispatch, getState: () => AppState) => {
+    const {
+      global: { wallet, ethereumNetworkTask, pendingDeposits },
+    } = getState();
+
+    if (
+      wallet !== undefined &&
+      (ethereumNetworkTask.status === "successful" || ethereumNetworkTask.status === "reloading")
+    ) {
+      const accountPendingDeposits = storage.getPendingDepositsByHermezAddress(
+        pendingDeposits,
+        ethereumNetworkTask.data.chainId,
+        wallet.hermezEthereumAddress
+      );
+      dispatch(transactionDetailsActionTypes.loadTransaction());
+
+      const transactionPromise: Promise<PendingDeposit | HistoryTransaction | PooledTransaction> =
+        new Promise((resolve, reject) => {
+          if (accountPendingDeposits !== undefined) {
+            const pendingDeposit = accountPendingDeposits.find(
+              (deposit) => deposit.hash === transactionIdOrHash
+            );
+            if (pendingDeposit !== undefined) {
+              resolve(pendingDeposit);
+            }
+          }
+
+          CoordinatorAPI.getPoolTransaction(transactionIdOrHash)
+            .then(resolve)
+            .catch((err: AxiosError) => {
+              if (err.response?.status === HttpStatusCode.NOT_FOUND) {
+                CoordinatorAPI.getHistoryTransaction(transactionIdOrHash)
+                  .then(resolve)
+                  .catch(reject);
+              } else {
+                reject(err);
+              }
+            });
+        });
+
+      return transactionPromise
+        .then((res: PendingDeposit | HistoryTransaction | PooledTransaction) => {
+          const txContainsBJJUserAddress =
+            "fromBJJ" in res &&
+            (res.fromBJJ === wallet.publicKeyBase64 || res.toBJJ === wallet.publicKeyBase64);
+          const txContainsHezEthereumAddressUserValue =
+            res.fromHezEthereumAddress === wallet.hermezEthereumAddress ||
+            res.toHezEthereumAddress === wallet.hermezEthereumAddress;
+
+          if (txContainsBJJUserAddress || txContainsHezEthereumAddressUserValue) {
+            dispatch(transactionDetailsActionTypes.loadTransactionSuccess(res));
+          } else {
+            dispatch(push("/"));
+          }
+        })
+        .catch(() => dispatch(transactionDetailsActionTypes.loadTransactionFailure()));
+    }
+  };
+}
+
+export { fetchTransaction };
