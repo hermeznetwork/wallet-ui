6d94bd288e71a862639486cde08e2bc7a85dca41
Alex Monso <amonsosanz@gmail.com>
2021-12-07 14:42:16 +0100

    Migrate to Typescript the component shared/private-route

--- src/views/shared/private-route/private-route.view.tsx
+++ src/views/shared/private-route/private-route.view.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Route, useLocation, Redirect } from "react-router-dom";
 
-import PrivateLayout from "../private-layout/private-layout.view";
+import PrivateLayout from "src/views/shared/private-layout/private-layout.view";
+import * as domain from "src/domain";
 
-function PrivateRoute({ isUserLoggedIn, route, onChangeRedirectRoute }) {
+interface PrivateRouteProps {
+  isUserLoggedIn: boolean;
+  route: domain.Route;
+  onChangeRedirectRoute: (route: string) => void;
+}
+
+function PrivateRoute({
+  isUserLoggedIn,
+  route,
+  onChangeRedirectRoute,
+}: PrivateRouteProps): JSX.Element {
   const { pathname, search } = useLocation();
 
   return (
