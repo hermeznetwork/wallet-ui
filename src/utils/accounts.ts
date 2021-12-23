31e6226654929271235346afca9488a8afdb649d
Alex Monso <amonsosanz@gmail.com>
2021-12-23 10:42:43 +0100

    Update ToDo comment on createAccount helper function

--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -59,7 +59,7 @@ function updateAccountToken(
   }
 }
 
-// TODO Study if this belongs to the domain model, as it's the function who creates a domain entity Account and move it there
+// ToDo: This helper should be moved to persistence when we move to persistence and abstract the calls to CoordinatorAPI.getAccounts
 function createAccount(
   account: HermezAccount,
   poolTransactions: PoolTransaction[] | undefined,
