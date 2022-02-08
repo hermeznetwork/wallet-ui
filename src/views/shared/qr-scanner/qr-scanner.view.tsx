f6d79682a29164c6a9654234e003e583d54d6fe9
Alex Monso <amonsosanz@gmail.com>
2022-02-08 11:10:33 +0100

    Change the library used to scan QR codes

--- src/views/shared/qr-scanner/qr-scanner.view.tsx
+++ src/views/shared/qr-scanner/qr-scanner.view.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import QrReader from "react-qr-reader";
+import { QrReader } from "@blackbox-vision/react-qr-reader";
 import { useTheme } from "react-jss";
 import { Addresses } from "@hermeznetwork/hermezjs";
 
@@ -9,7 +9,6 @@ import Container from "../container/container.view";
 import { ReactComponent as QRScannerMask } from "../../../images/qr-scanner-mask.svg";
 import { ReactComponent as ArrowBackIcon } from "../../../images/icons/arrow-back.svg";
 import { ReactComponent as QRCodeIcon } from "../../../images/icons/qr-code.svg";
-import Spinner from "../spinner/spinner.view";
 import Button from "../button/button.view";
 import { Theme } from "src/styles/theme";
 
@@ -22,25 +21,13 @@ interface QRScannerProps {
 function QRScanner({ hideMyCode, onSuccess, onClose }: QRScannerProps): JSX.Element {
   const theme = useTheme<Theme>();
   const classes = useQRScannerStyles();
-  const [isQRScannerLoaded, setIsQRScannerLoaded] = React.useState(false);
 
   /**
-   * Handles the onLoad event from the QrReader component. It sets the isQRScannerLoaded
-   * to true when the QR Scanner has been loaded in the DOM
-   */
-  function handleQRScanLoad() {
-    setIsQRScannerLoaded(true);
-  }
-
-  /**
-   * Handles the onScan event from the QrReader component. If the result is a valid
+   * Handles the scan event from the QrReader component. If the result is a valid
    * hermez address it bubbles up the read value to the parent component
    */
-  function handleQRScan(result: string | null) {
-    if (
-      result &&
-      (Addresses.isHermezEthereumAddress(result) || Addresses.isEthereumAddress(result))
-    ) {
+  function handleQRScan(result: string) {
+    if (Addresses.isHermezEthereumAddress(result) || Addresses.isEthereumAddress(result)) {
       onSuccess(result);
     }
   }
@@ -52,46 +39,44 @@ function QRScanner({ hideMyCode, onSuccess, onClose }: QRScannerProps): JSX.Elem
     console.error(error);
   }
 
+  const NO_QR_CODE_FOUND_ERROR = "NotFoundException";
+
   return (
     <Portal>
       <div className={classes.section}>
         <Container disableGutters backgroundColor={theme.palette.primary.main}>
           <div className={classes.sectionContent}>
-            {!isQRScannerLoaded && (
-              <div className={classes.spinnerWrapper}>
-                <Spinner />
-              </div>
-            )}
-            {/* ToDo: Fix QR reading */}
-            <div
-              // showViewFinder={false}
+            <QrReader
               className={classes.qrReaderWrapper}
-              onLoad={handleQRScanLoad}
-              // onScan={handleQRScan}
-              onError={handleQRScanError}
+              onResult={(result, error) => {
+                if (result) {
+                  handleQRScan(result.getText());
+                }
+                if (error && error.name !== NO_QR_CODE_FOUND_ERROR) {
+                  handleQRScanError(error);
+                }
+              }}
+              // We have to pass in the default value because the prop is not optional ¯\_(ツ)_/¯
+              constraints={{ facingMode: "user" }}
             />
-            {isQRScannerLoaded && (
-              <>
-                <div className={classes.qrReaderFrame}>
-                  <QRScannerMask className={classes.qrScannerMask} />
-                </div>
-                <div className={classes.goBackButtonWrapper}>
-                  <Container disableVerticalGutters>
-                    <button className={classes.goBackButton} onClick={onClose}>
-                      <ArrowBackIcon className={classes.goBackButtonIcon} />
-                    </button>
-                  </Container>
-                </div>
-                <div className={classes.myCodeButtonWrapper}>
-                  {!hideMyCode && (
-                    <>
-                      <Button Icon={<QRCodeIcon />} onClick={onClose} />
-                      <p className={classes.myCodeLabel}>My Code</p>
-                    </>
-                  )}
-                </div>
-              </>
-            )}
+            <div className={classes.qrReaderFrame}>
+              <QRScannerMask className={classes.qrScannerMask} />
+            </div>
+            <div className={classes.goBackButtonWrapper}>
+              <Container disableVerticalGutters>
+                <button className={classes.goBackButton} onClick={onClose}>
+                  <ArrowBackIcon className={classes.goBackButtonIcon} />
+                </button>
+              </Container>
+            </div>
+            <div className={classes.myCodeButtonWrapper}>
+              {!hideMyCode && (
+                <>
+                  <Button Icon={<QRCodeIcon />} onClick={onClose} />
+                  <p className={classes.myCodeLabel}>My Code</p>
+                </>
+              )}
+            </div>
           </div>
         </Container>
       </div>
